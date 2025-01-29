998bf29c8fe04a3ba04a575e0cb6925ab08e40bf
alvin liu <glitteryash@gmail.com>
2025-01-29 13:16:07 +0900

    register user

--- server/index.js
+++ server/index.js
@@ -23,6 +23,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api/user", authRoute);
 
+app.get("/", (req, res) => {
+  res.send("homepage");
+});
+
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const registerValidation = require("../validation").registerValidation;
+const User = require("../models").userModel;
 
 router.use((req, res, next) => {
   console.log("A request is coming in to /auth");
@@ -11,10 +12,29 @@ router.get("/testAPI", (req, res) => {
   return res.json(msgObj);
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   console.log("register route is working");
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+  // check if the user is already in the database
+  const emailExist = await User.findOne({ email: req.body.email });
+  if (emailExist) return res.status(400).send("Email has already been used.");
+  // create a new user
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password,
+    role: req.body.role,
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(200).send({
+      msg: "User has been saved successfully",
+      data: savedUser,
+    });
+  } catch (err) {
+    res.status(400).send("User could not be saved", err);
+  }
 });
 
 module.exports = router;
