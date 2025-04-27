05b3d648bc1c3db3b370ccd6ab8a6cc2e9f2fa63
desk-059 <hacchobori-05@desk-059.local>
2025-04-27 17:05:50 +0900

    update

--- client/src/services/course.service.js
+++ client/src/services/course.service.js
@@ -1,7 +1,5 @@
 import axios from "axios";
 const API_URL = `${process.env.REACT_APP_API_URL}/courses`;
-// const API_URL = `${process.env.REACT_APP_API_URL}/courses`;
-console.log("API_URL", API_URL);
 
 class CourseService {
   post(title, description, price, courseImage) {
